666568284a2ce3b87111e2b13be2f159e3b25c89
Kush Sharma <kush.73888@gmail.com>
2024-01-23 19:24:14 +0530

    Gallery Page

--- src/app/gallery/page.tsx
+++ src/app/gallery/page.tsx
@@ -1,17 +1,34 @@
-'use client';
 import UploadButton from "./upload-button";
-import { Button } from '@/components/ui/button';
-import { CldUploadButton } from 'next-cloudinary';
+import cloudinary from 'cloudinary';
+import GalleryGrid from "./gallery-grid";
+import { SearchForm } from "./search-form";
 
-export default function Gallery() {
-    return ( 
-    <section>
-       <div className="flex flex-col gap-8">
-        <div className="flex justify-between">
-          <h1 className="text-4xl font-bold">Gallery</h1>
-           <UploadButton />
-        </div>
-        </div>
+
+export type SearchResult = {
+    public_id: string;
+    tags: string[];
+  };
+  
+
+export default async function Gallery() {
+  const results = await cloudinary.v2.search
+  .expression('resource_type:image')
+  .sort_by('public_id', 'desc')
+  .max_results(5)
+  .execute() as { resources: SearchResult[]};
+  console.log(results);
+
+return ( 
+   <section>
+    <div className="flex flex-col gap-8">
+     <div className="flex justify-between">
+     <h1 className="text-4xl font-bold">Gallery</h1>
+         <UploadButton />
+    </div>
+
+    <SearchForm initialSearch=""/>
+    <GalleryGrid images={results.resources} />
+    </div>
     </section>
     );
 }
